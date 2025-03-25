ddabf0967ad51428073d10a1b17fc6ad337f534e
THEHYPER-ROBOT <THEHYPER1232@gmail.com>
2025-03-26 01:32:57 +0530

    Update config.py

--- config.py
+++ config.py
@@ -36,7 +36,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/THEHYPER-ROBOT/ACX_Music",
+    "https://github.com/Learningbots79/LB_Music",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
 GIT_TOKEN = getenv(
